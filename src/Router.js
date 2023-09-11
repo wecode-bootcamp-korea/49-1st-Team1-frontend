4f93deff1c03ac8114c3bdac31d412717b5cb9cc
jongwan Kim <aegis0918@naver.com>
2023-09-11 19:25:56 +0900

    restart

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login/login";
+import Login from "../src/pages/Login/login";
 import Main from "./pages/threadMainPage/main";
 import Write from "./pages/threadWritePage/write";
 import Signup from "./pages/userSignUp/signUp";
