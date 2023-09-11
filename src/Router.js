64c293f4ac83026c81e9933a858267a4dbf6de4f
jongwan Kim <aegis0918@naver.com>
2023-09-11 18:30:34 +0900

    changed folder

--- src/Router.js
+++ src/Router.js
@@ -1,1 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/login";
+import Main from "./pages/threadMainPage/main";
+import Write from "./pages/threadWritePage/write";
+import Signup from "./pages/userSignUp/signUp";
 
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/write" element={<Write />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default Router;
