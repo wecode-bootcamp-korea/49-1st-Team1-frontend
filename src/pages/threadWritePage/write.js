a7553b37e2391679b2276df7403e1538a122e7b1
jongwan Kim <aegis0918@naver.com>
2023-09-13 20:21:10 +0900

    thread_main

--- src/pages/threadWritePage/write.js
+++ src/pages/threadWritePage/write.js
@@ -1,35 +1,40 @@
 import React, { useEffect, useState } from "react";
 import "./write.scss";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Write = () => {
   const navigate = useNavigate();
 
+  //   const {parameter} = useParams();
+
   const [imageURL, setImageURL] = useState("");
   const [threadContent, setThreadContent] = useState("");
   const [userNickname, setUserNickName] = useState("");
-  const [loginToken, setLoginToken] = useState("");
+  const [checkedThread, setCheckedThread] = useState(false);
+  const [userId, setUserId] = useState("");
 
-  useEffect(() => {
-    fetch("엔드포인트 URL", { method: "GET" })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("이미지 URL 가져오기 실패");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setImageURL(data.imageURL);
-      })
-      .catch((error) => {
-        alert("이미지 URL 가져오기 실패하였습니다.");
-      });
-  }, []);
+  const loginToken = localStorage.getItem("token");
+
+  //   useEffect(() => {
+  //     fetch("", { method: "GET" })
+  //       .then((response) => {
+  //         if (!response.ok) {
+  //           throw new Error("이미지 URL 가져오기 실패");
+  //         }
+  //         return response.json();
+  //       })
+  //       .then((data) => {
+  //         setImageURL(data.imageURL);
+  //       })
+  //       .catch((error) => {
+  //         alert("이미지 URL 가져오기 실패하였습니다.");
+  //       });
+  //   }, []);
 
   const fetchFunction = () => {
     if (loginToken) {
       if (threadContent) {
-        fetch("http://10.58.52.222:8000/thread", {
+        fetch("http://10.58.52.215:8000/threads/writeThread", {
           method: "POST",
           headers: {
             "Content-Type": "application/json;charset=utf-8",
@@ -49,6 +54,55 @@ const Write = () => {
       alert("로그인이 필요합니다.");
     }
   };
+
+  //   const ThreadUpdate = () => {
+  //     if (loginToken) {
+  //       if (threadContent) {
+  //         fetch("http://10.58.52.222:8000/thread", {
+  //           method: "POST",
+  //           headers: {
+  //             "Content-Type": "application/json;charset=utf-8",
+  //             Authorization: loginToken,
+  //           },
+  //           body: JSON.stringify({
+  //             content: threadId,
+  //           }),
+  //         })
+  //           .then((response) => {
+  //             if (!response.ok) {
+  //               throw new Error("게시글 수정 실패");
+  //             }
+  //             return response.json();
+  //           })
+  //           .then((result) => {
+  //             console.log(result);
+  //             navigate(`/thread/${"쓰레드 아이디"}`); // 수정 완료 후 해당 게시글 페이지로 이동
+  //           })
+  //           .catch((error) => {
+  //             console.error(error);
+  //             alert("게시글 수정 실패하였습니다.");
+  //           });
+  //       } else {
+  //         alert("내용을 확인해 주세요.");
+  //       }
+  //     } else {
+  //       alert("로그인이 필요합니다.");
+  //     }
+  //   };
+
+  //   const UpdateSending = () => {
+  //     fetch(`http://10.58.52.222:8000/thread/${쓰레드아이디}`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json;charset=utf-8",
+  //       },
+  //       body: JSON.stringify({
+  //         content: threadContent,
+  //       }),
+  //     })
+  //       .then((reponse) => {})
+  //       .then((result) => {});
+  //   };
 
   return (
     <div className="thread_container">
@@ -74,7 +128,7 @@ const Write = () => {
           </div>
         </div>
         <div className="thread_btn">
-          <button className="cancel_btn" onClick={() => navigate("/main")}>
+          <button className="cancel_btn" onClick={() => navigate("/")}>
             취소
           </button>
           <button className="confirm_btn" onClick={fetchFunction}>
--- src/pages/userSignUp/signUp.js
+++ src/pages/userSignUp/signUp.js
@@ -4,209 +4,224 @@ import { useNavigate } from "react-router-dom";
 import SuccessMessage from "./successMessage";
 
 const Signup = () => {
-    const [userInfo, setUserInfo] = useState();
-    const [success, setSuccess] = useState(false);
-    const [userPhoneNumber, setUserPhoneNumber] = useState({
-        phoneNumber: "010",
-        phone: "",
+  const [userInfo, setUserInfo] = useState();
+  const [success, setSuccess] = useState(false);
+  const [userPhoneNumber, setUserPhoneNumber] = useState({
+    phoneNumber: "010",
+    phone: "",
+  });
+
+  const handlePhoneNumber = (e) => {
+    const selectedPhoneNumber = e.target.value;
+    console.log(selectedPhoneNumber);
+    setUserPhoneNumber({
+      ...userPhoneNumber,
+      phonNumber: selectedPhoneNumber,
     });
+  };
 
-    const handlePhoneNumber = (e) => {
-        const selectedPhoneNumber = e.target.value;
-        console.log(selectedPhoneNumber);
-        setUserPhoneNumber({
-            ...userPhoneNumber,
-            phonNumber: selectedPhoneNumber,
-        });
-    };
+  const handlePhoneInput = (e) => {
+    const phoneValue = e.target.value;
+    setUserInfo({
+      ...userInfo,
+      phone: phoneValue,
+    });
+  };
 
-    const handlePhoneInput = (e) => {
-        const phoneValue = e.target.value;
-        setUserInfo({
-            ...userInfo,
-            phone: phoneValue,
-        });
-    };
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const goLogin = () => {
+    navigate("/Login");
+  };
 
-    const goLogin = () => {
-        navigate("/Login");
-    };
+  const handleUserInfo = (e) => {
+    const { name, value } = e.target;
 
-    const handleUserInfo = (e) => {
-        const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+  console.log(userInfo);
 
-        setUserInfo({ ...userInfo, [name]: value });
-    };
-    console.log(userInfo);
+  const fowardPhoneNumberFunction = () => {
+    const newNumberArray = [];
+    const fowardPhoneNumber = ["010", "011", "016", "019"];
+    for (let i = 0; i < fowardPhoneNumber.length; i++) {
+      newNumberArray.push(
+        <option key={fowardPhoneNumber[i]}>{fowardPhoneNumber[i]}</option>
+      );
+    }
+    console.log(newNumberArray);
+    return newNumberArray;
+  };
 
-    // const createYear = () => {
-    //     const newYear = [];
-    // };
+  const CreateYear = () => {
+    const listYear = [];
+    const CreateYear = new Date();
+    for (let i = 1920; i <= CreateYear.getFullYear(); i++) {
+      listYear.push(<option key={i}>{i} 년</option>);
+    }
+    return listYear;
+  };
 
-    const createMonth = () => {
-        const listMonth = [];
-        for (let i = 1; i <= 12; i++) {
-            listMonth.push(<option key={i}>{i}월</option>);
-        }
-        return listMonth;
-    };
+  const createMonth = () => {
+    const listMonth = [];
+    for (let i = 1; i <= 12; i++) {
+      listMonth.push(<option key={i}>{i}월</option>);
+    }
+    return listMonth;
+  };
 
-    const createDay = () => {
-        const listDay = [];
-        for (let i = 1; i <= 31; i++) {
-            listDay.push(<option key={i}>{i}일</option>);
-        }
-        return listDay;
-    };
-    //이메일, 비밀번호 정규식
-    const isEmailValid = (email) => {
-        const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-        return emailRegex.test(email);
-    };
+  const createDay = () => {
+    const listDay = [];
+    for (let i = 1; i <= 31; i++) {
+      listDay.push(<option key={i}>{i}일</option>);
+    }
+    return listDay;
+  };
+  //이메일, 비밀번호 정규식
+  const isEmailValid = (email) => {
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    return emailRegex.test(email);
+  };
 
-    const isPasswordValid = (password) => {
-        return password.length >= 10;
-    };
+  const isPasswordValid = (password) => {
+    return password.length >= 10;
+  };
 
-    const isButtonEnabled = (e) => {
-        e.preventDefault();
-        const { email, password, "confirm-password": confirmPassword } = userInfo;
+  const isButtonEnabled = (e) => {
+    e.preventDefault();
+    const { email, password, "confirm-password": confirmPassword } = userInfo;
 
-        if (
-            !userInfo ||
-            !userInfo["email"] ||
-            !userInfo["password"] ||
-            !userInfo["confirm-password"]
-        ) {
-            alert("모든 필드를 채워주세요.");
-        } else if (!isEmailValid(email)) {
-            alert("올바른 이메일 형식이 아닙니다.");
-        } else if (!isPasswordValid(password)) {
-            alert("비밀번호는 10자리 이상으로 작성해 주세요.");
-        } else if (userInfo["password"] !== userInfo["confirm-password"]) {
-            alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
-        } else {
-            setSuccess(true);
-        }
+    if (
+      !userInfo ||
+      !userInfo["email"] ||
+      !userInfo["password"] ||
+      !userInfo["confirm-password"]
+    ) {
+      alert("모든 필드를 채워주세요.");
+    } else if (!isEmailValid(email)) {
+      alert("올바른 이메일 형식이 아닙니다.");
+    } else if (!isPasswordValid(password)) {
+      alert("비밀번호는 10자리 이상으로 작성해 주세요.");
+    } else if (userInfo["password"] !== userInfo["confirm-password"]) {
+      alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+    } else {
+      setSuccess(true);
+    }
 
-        fetch("http://10.58.52.222:8000/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=utf-8",
-            },
-            body: JSON.stringify(userInfo, userPhoneNumber),
-        })
-            .then((res) => res.json())
-            .then((result) => {
-                console.log(result);
+    fetch("http://10.58.52.222:8000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(userInfo, userPhoneNumber),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
 
-                // if (result.message === "LOGIN_SUCCESS") {
-                //     alert("성공이다 !");
-                // }
-            });
-    };
+        // if (result.message === "LOGIN_SUCCESS") {
+        //     alert("성공이다 !");
+        // }
+      });
+  };
 
-    return (
-        <div className="Signup">
-            <div className="inner-box">
-                <div className="btn-back-wrapper">
-                    <button onClick={goLogin} className="btn-back"></button>
-                </div>
-                <form className="join-form">
-                    {success && <SuccessMessage onClose={() => setSuccess(false)} />}
-                    <h2>회원가입</h2>
-                    <div className="input-wrapper mt-24">
-                        <label>
-                            <span>기본 정보</span>
-                            <span className="color-red note">필수 사항</span>
-                        </label>
-                        <input
-                            type="text"
-                            name="email"
-                            placeholder="이메일"
-                            onChange={handleUserInfo}
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            placeholder="비밀번호"
-                            onChange={handleUserInfo}
-                        />
-                        <input
-                            type="password"
-                            name="confirm-password"
-                            placeholder="비밀번호 확인"
-                            onChange={handleUserInfo}
-                        />
-                    </div>
-                    <div className="input-wrapper mt-24">
-                        <label>
-                            <span>닉네임과 프로필 이미지</span>
-                            <span className="note">선택 사항</span>
-                        </label>
-                        <input
-                            type="text"
-                            name="nickname"
-                            placeholder="닉네임"
-                            onChange={handleUserInfo}
-                        />
-                    </div>
-                    <div className="input-wrapper input-phonenumber mt-24">
-                        <label>
-                            <span>전화번호</span>
-                            <span className="note">선택 사항</span>
-                        </label>
-                        <div className="input-inner">
-                            <select
-                                className="phone-number"
-                                id="phone-number"
-                                value={userPhoneNumber.phoneNumber}
-                                onChange={handlePhoneNumber}
-                            >
-                                <option value="010">010</option>
-                                <option value="019">019</option>
-                                <option value="016">016</option>
-                                <option value="011">011</option>
-                            </select>
-                            <input
-                                id="phone"
-                                type="tel"
-                                name="phone-number"
-                                placeholder="휴대폰 번호를 입력해주세요."
-                                //value={userPhoneNumber.phone}
-                                onChange={handlePhoneInput}
-                            />
-                        </div>
-                    </div>
-                    <div className="input-wrapper mt-24">
-                        <label id="">
-                            <span>생일</span>
-                            <span className="note">선택 사항</span>
-                        </label>
-                        <div className="input-inner">
-                            <select id="year" className="year">
-                                <option value="2000">2000년</option>
-                                <option value="1999">1999년</option>
-                                <option value="1998">1998년</option>
-                                <option value="1997">1997년</option>
-                            </select>
-                            <select id="month" className="month">
-                                {createMonth()}
-                            </select>
-                            <select id="day" className="day">
-                                {createDay()}
-                            </select>
-                        </div>
-                    </div>
-                    <button type="submit" className="btn-join-b" onClick={isButtonEnabled}>
-                        회원가입
-                    </button>
-                </form>
-            </div>
+  return (
+    <div className="Signup">
+      <div className="inner-box">
+        <div className="btn-back-wrapper">
+          <button onClick={goLogin} className="btn-back"></button>
         </div>
-    );
+        <form className="join-form">
+          {success && <SuccessMessage onClose={() => setSuccess(false)} />}
+          <h2>회원가입</h2>
+          <div className="input-wrapper mt-24">
+            <label>
+              <span>기본 정보</span>
+              <span className="color-red note">필수 사항</span>
+            </label>
+            <input
+              type="text"
+              name="email"
+              placeholder="이메일"
+              onChange={handleUserInfo}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="비밀번호"
+              onChange={handleUserInfo}
+            />
+            <input
+              type="password"
+              name="confirm-password"
+              placeholder="비밀번호 확인"
+              onChange={handleUserInfo}
+            />
+          </div>
+          <div className="input-wrapper mt-24">
+            <label>
+              <span>닉네임과 프로필 이미지</span>
+              <span className="note">선택 사항</span>
+            </label>
+            <input
+              type="text"
+              name="nickname"
+              placeholder="닉네임"
+              onChange={handleUserInfo}
+            />
+          </div>
+          <div className="input-wrapper input-phonenumber mt-24">
+            <label>
+              <span>전화번호</span>
+              <span className="note">선택 사항</span>
+            </label>
+            <div className="input-inner">
+              <select
+                className="phone-number"
+                id="phone-number"
+                value={userPhoneNumber.phoneNumber}
+                onChange={handlePhoneNumber}
+              >
+                {fowardPhoneNumberFunction()}
+              </select>
+              <input
+                id="phone"
+                type="tel"
+                name="phone-number"
+                placeholder="휴대폰 번호를 입력해주세요."
+                //value={userPhoneNumber.phone}
+                onChange={handlePhoneInput}
+              />
+            </div>
+          </div>
+          <div className="input-wrapper mt-24">
+            <label id="">
+              <span>생일</span>
+              <span className="note">선택 사항</span>
+            </label>
+            <div className="input-inner">
+              <select id="year" className="year">
+                {CreateYear()}
+              </select>
+              <select id="month" className="month">
+                {createMonth()}
+              </select>
+              <select id="day" className="day">
+                {createDay()}
+              </select>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="btn-join-b"
+            onClick={isButtonEnabled}
+          >
+            회원가입
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
