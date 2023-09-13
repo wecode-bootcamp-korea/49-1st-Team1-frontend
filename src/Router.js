e57de11b103c371e9dd1ed5a73ca4dcf1cfb612a
jongwan Kim <aegis0918@naver.com>
2023-09-13 13:59:09 +0900

    write page create

--- src/Router.js
+++ src/Router.js
@@ -7,16 +7,16 @@ import Signup from "./pages/userSignUp/signUp";
 import SuccessMessage from "./pages/userSignUp/successMessage";
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/write" element={<Write />} />
-                <Route path="/successMessage" element={<SuccessMessage />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/successMessage" element={<SuccessMessage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default Router;
